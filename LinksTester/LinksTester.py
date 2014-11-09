f13bd7b13ec905f6c339a708588853e9615deb69
karim <karim.ouahab@gmail.com>
2014-11-09 15:35:47 +0800

    Add comments. Externalize ping and ssh commands. Improved output

--- LinksTester/LinksTester.py
+++ LinksTester/LinksTester.py
@@ -1,7 +1,7 @@
 '''
-Generates an RTT matrix from an arbitrary number of machines
-Detect significant deviations from a reference matrix
-Send a report of the analysis by email
+- Generates an RTT matrix from an arbitrary number of machines
+- Detect significant deviations from a reference matrix
+- Send a report of the analysis by email
 
 Created on Nov 6, 2014
 
@@ -18,19 +18,17 @@ import smtplib
 from email.mime.text import MIMEText
 
 #global variables
-machines    = None
-pingMatrix  = collections.defaultdict()
-refMatrix   = collections.defaultdict()
+machines                    = None
+pingMatrix                  = collections.defaultdict()
+refMatrix                   = collections.defaultdict()
 cmdLineOptions              = None
 jsonconfig                  = None
 PingOutputToMicroFactor     = 1000
-pingCmd                     = "ping"
-sshCmd                      = "ssh"
 pingOKAlerts                = list()
 pingNOKAlerts               = list()
 allErrors                   = list()
 
-PING_NA = "N/A"
+PING_NA     = "N/A"
 
 OK_COLOR    = "#C1FFC1"
 NOK_COLOR   = "#FF0000"
@@ -79,6 +77,8 @@ class PingAlert():
         diff = 100 * (1 - float(self.ping) / float(self.refPing))
         return "RTT from {} to {} ".format(self.srcMachine.datacenter, self.tgtMachine.datacenter) + ("increased" if diff < 0 else "decreased") + " : from {} us to {} us ({} %)".format(self.refPing, self.ping, "{0:.1f}".format(diff))
 
+"""Parse the json configuration file. The configuration node is stored in 'jsonconfig'
+"""  
 def parseConfigurationFile(fileName):
     global machines, jsonconfig
     if not cmdLineOptions.quiet:
@@ -87,6 +87,8 @@ def parseConfigurationFile(fileName):
         jsonconfig  = json.load(machinesFile)["configuration"]
         machines    = jsonconfig["machines"]
 
+"""Parse the reference average rtt file. The result is stored in refMatrix
+""" 
 def parseReferenceFile(fileName):
     if not cmdLineOptions.quiet:
         print "Getting reference rtt from " + fileName
@@ -100,6 +102,9 @@ def parseReferenceFile(fileName):
                 refMatrix[srcMachine] = dict()
             refMatrix[srcMachine][tgtMachine] = ping
 
+""" Retrieve the reference average rtt between the two given Machine
+    If non is found, N/A is returned
+""" 
 def getReferencePing(src, tgt):
     if refMatrix.has_key(src) and refMatrix[src].has_key(tgt):
         return refMatrix[src][tgt];
@@ -117,7 +122,7 @@ def generateReferenceFile(matrix, fileName):
                 refFile.write("{}->{} : {}\n".format(src, tgt, matrix[src][tgt].avg))
 
 def parseConfiguration():
-    global cmdLineOptions, pingCmd
+    global cmdLineOptions
     parser = OptionParser()
     
     parser.add_option("-f", "--file", dest="config_filename",
@@ -136,9 +141,6 @@ def parseConfiguration():
         parser.print_help()
         exit(1)
     parseConfigurationFile(cmdLineOptions.config_filename)
-    pingCmd += " -q -c " + str(jsonconfig["ping_count"]) + " -w" + str(jsonconfig["ping_timeout"])
-    if not cmdLineOptions.quiet:
-        print "ping command is : " + pingCmd
     ref_rtt_filename = jsonconfig["reference_file"]
     if "" == ref_rtt_filename:
         print "Please provide a reference rtt filename"
@@ -147,6 +149,8 @@ def parseConfiguration():
     if not cmdLineOptions.genReference:
         parseReferenceFile(ref_rtt_filename)
 
+"""Parse the text output of a ping command, returns a Ping instance representing that output.
+""" 
 def parsePingOutput(pingStr):
     try:
         match = re.search('([\d]*\.[\d]*)/([\d]*\.[\d]*)/([\d]*\.[\d]*)/([\d]*\.[\d]*)', pingStr)
@@ -160,9 +164,17 @@ def parsePingOutput(pingStr):
         return Ping(PING_NA, PING_NA, PING_NA, PING_NA)
 
 
+"""Main ping procedure : execute ssh/ping command over all the links.
+   Returns the ping matrix
+""" 
 def executePings():
     sshUser     = jsonconfig["ssh_user"]
+    sshCmd      = jsonconfig["ssh_cmd"]
     sshOptions  = jsonconfig["ssh_options"]
+    pingCmd     = jsonconfig["ping_cmd"] + " -q -c " + str(jsonconfig["ping_count"]) + " -w " + str(jsonconfig["ping_timeout"])
+    if not cmdLineOptions.quiet:
+        print "ping command is : " + pingCmd
+    
     for source in machines:
         for target in machines:
             srcHost         = source["host"]
@@ -175,9 +187,9 @@ def executePings():
                 output = "-"
             else:
                 try:
-                    output = subprocess.check_output("{} {} {}@localhost {} {} | grep rtt".format(sshCmd, sshOptions, sshUser, pingCmd, tgtHost) , shell=True)
+                    output = subprocess.check_output("{} {} {}@{} {} {} | grep rtt".format(sshCmd, sshOptions, sshUser, srcHost, pingCmd, tgtHost) , shell=True)
                 except Exception as e:
-                    allErrors.append("An error occurred will trying to ping from {} to {} : {}".format(srcHost, tgtHost, e))
+                    allErrors.append("An error occurred while trying to ping from {} to {} : {}".format(srcHost, tgtHost, e))
                     output = ""
             if not pingMatrix.has_key(srcMachine):
                 pingMatrix[srcMachine] = dict()
@@ -187,6 +199,8 @@ def executePings():
         generateReferenceFile(pingMatrix, jsonconfig["reference_file"])
     return pingMatrix            
 
+"""In non quiet mode, output the ping matrix to the standard output
+""" 
 def printPings(results):
     if cmdLineOptions.quiet:
         return
@@ -194,6 +208,8 @@ def printPings(results):
         for tgt in results[src]:
             print str(results[src][tgt])
 
+"""Returns an HTML view of all the ping alert messages
+""" 
 def getPingAlertHtmlMessages():
     result = ""
     if len(pingNOKAlerts) > 0:
@@ -205,9 +221,13 @@ def getPingAlertHtmlMessages():
         result += "<b><font color=" + BET_COLOR + ">Improvements:</font></b><ul>"
         for alert in pingOKAlerts:
             result += "<li>" + str(alert) + '</li><br>'
-        result += "</ul>"        
+        result += "</ul>"
+    if not result == "":
+        result += "<br><i>Since changes have been detected, you may want to reset the reference rtt (-g option), or (unlikely) increase the deviation tolerance (current value :" + str(jsonconfig["deviation_percent"]) +" %) </i>"       
     return  result
 
+"""Returns an HTML view of all the error messages (an error is NOT a ping alert)
+""" 
 def getAllErrorsHtmlMessages():
     result = ""
     if len(allErrors) > 0:
@@ -224,7 +244,9 @@ def isfloat(string):
     except:
         return False
 
-def getHtmlComparisonToReference(srcMachine, tgtMachine, newPing, refPing):
+def getHtmlComparisonToReference(srcMachine, tgtMachine, results):
+    newPing = results[srcMachine][tgtMachine]
+    refPing = getReferencePing(srcMachine, tgtMachine)
     diff = 0
     deviationPercent = jsonconfig["deviation_percent"]
     if isfloat(newPing.avg) and isfloat(refPing.avg):
@@ -240,26 +262,36 @@ def getHtmlComparisonToReference(srcMachine, tgtMachine, newPing, refPing):
 
 def generateHtmlTable(results):
     htmlTable = '<table border="1" style="width:100%" cellspacing="1" cellpadding="0" border="0" align="center" bgcolor="#999999">'
-    htmlTable += "<tr><td></td>"
+    #Draw the header line
+    htmlTable += "<tr><td align='center'>From / To</td>"
     for src in results:
         htmlTable += "<td align='center' bgcolor='#DBDBDB'><b>"
-        htmlTable += "{} [{}]".format(src.datacenter, src.hostname)
+        if jsonconfig["display_only_datacenters"]:
+            htmlTable += "{}".format(src.datacenter)
+        else:
+            htmlTable += "{} [{}]".format(src.datacenter, src.hostname)
         htmlTable += "</b></td>"
     htmlTable += "</tr>"
+    #Draw all the rows
     for src in results:
         htmlTable += "<tr>"
         htmlTable += "<td align='center' bgcolor='#DBDBDB'><b>"
-        htmlTable += "{} [{}]".format(src.datacenter, src.hostname)
+        if jsonconfig["display_only_datacenters"]:
+            htmlTable += "{}".format(src.datacenter)
+        else:
+            htmlTable += "{} [{}]".format(src.datacenter, src.hostname)
         htmlTable += "</b></td>"
         for tgt in results[src]:
             htmlTable += "<td align='center' "
-            htmlTable += getHtmlComparisonToReference(src, tgt, results[src][tgt], getReferencePing(src, tgt))
+            htmlTable += getHtmlComparisonToReference(src, tgt, results)
             htmlTable += "</td>"  
         htmlTable += "</tr>"
             
     htmlTable += "</table>"
     return htmlTable
 
+"""Given a reference and a new HTML version of the RTT tables, generate the final HTML body
+""" 
 def generateOutput(newTable, refTable):
     htmlOut = '''  
 <html>  
@@ -301,7 +333,11 @@ def sendReport(htmlReport):
     # Create a html message
     msg = MIMEText(htmlReport, 'html')
     
-    msg['Subject']  = "[ALERT][DataCenters links] {} regressions - {} improvements".format(len(pingNOKAlerts), len(pingOKAlerts))
+    msg['Subject']  = "[ALERT][Network Lines] {} regression{} - {} improvement{}".format(
+                                                                                         len(pingNOKAlerts),
+                                                                                         "s" if len(pingNOKAlerts) > 1 else "",
+                                                                                         len(pingOKAlerts),
+                                                                                         "s" if len(pingOKAlerts) > 1 else "")
     msg['From']     = fromMail
     msg['To']       = toMail
     
@@ -318,7 +354,7 @@ if __name__ == '__main__':
     parseConfiguration()
     newRun = generateHtmlTable(executePings())
     
-    if cmdLineOptions.genReference: #don't generate a report if a new ref is generated
+    if cmdLineOptions.genReference: #don't generate a report if a new reference file is being generated
         exit(0)
     
     refRun = generateHtmlTable(refMatrix)
